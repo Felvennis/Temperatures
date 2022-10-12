9af0c5dbb404ef4b97f32150339bb2e6ecf97822
gable <gable@MSI>
2022-10-12 14:19:52 +0200

    Ajoutez des fichiers projet.

---
+++ Temperatures/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temperatures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int taille = 5;
+            float[] t = new float[taille];
+
+            float moyenne = 0;
+            for (int k =0; k < taille; k++)
+            {
+                Console.Write("Entrez la température n°" + (k + 1) + "=");
+                t[k] = float.Parse(Console.ReadLine());
+                moyenne += t[k];
+            }
+            moyenne = moyenne / taille;
+
+            int nbsup = 0;
+            for (int k =0; k < taille; k++)
+            {
+                if (t[k] > moyenne)
+                {
+                    nbsup++;
+                }
+            }
+            Console.WriteLine("Nombre de t° au-dessus de la moyenne = " + nbsup);
+            Console.ReadLine();
+        }
+    }
+}
